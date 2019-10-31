c2371e4335efcdc5870dad4925bca25219b9619b
MorganeBeCode <morgane.jansen@gmail.com>
2019-10-31 16:58:09 +0100

    ajout structure et css

---
+++ index.php
@@ -0,0 +1,180 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <!--Import Google Icon Font-->
+  <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+  <!--Import materialize.css-->
+  <link type="text/css" rel="stylesheet" href="assets/css/materialize.css" media="screen,projection" />
+  <!--Fontawesome-->
+  <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+  <!--Favicon-->
+  <link rel="shortcut icon" href="assets/img/favicon.ico" type="image/x-icon">
+  <title>Contact Form</title>
+</head>
+
+<body>
+  <!--Header-->
+  <header role=”banner”>
+    <nav role=”navigation” id="header-nav">
+      <div class="nav-wrapper">
+        <img class="brand-logo center" id="logo" src="assets/img/hackers-poulette-logo.png" alt="logo">
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="sass.html">Home</a></li>
+          <li><a href="badges.html">About</a></li>
+          <li><a href="collapsible.html">Shop</a></li>
+        </ul>
+      </div>
+    </nav>
+  </header>
+
+  <!--Form-->
+  <main role="main">
+    <div class="container">
+      <div class="row">
+        <form class="col s12" method="post" action="form.php">
+          <h3>Contact us</h3>
+          <div class="row">
+            <!--Lastname input-->
+            <div class="input-field col s5">
+              <label for="lastname">Last Name</label>
+              <input name="lastname" id="last_name" type="text" class="validate" required aria-required="true">
+            </div>
+            <!--Firstname input-->
+            <div class="input-field col s5 offset-s1">
+              <label for="firstname">First Name</label>
+              <input name="firstname" id="first_name" type="text" class="validate" required aria-required="true">
+            </div>
+            <!--Gender radio buttons-->
+            <div class="input-field col s5">
+              <fieldset>
+                <legend>Gender</legend>
+                <label for="male" id="genders">
+                  <input type="radio" name="gender" value="male" id="male" required aria-required="true" />
+                  <span>Male</span>
+                </label>
+                <label for="female" id="genders">
+                  <input type="radio" name="gender" value="female" id="female" />
+                  <span>Female</span>
+                </label>
+                <label for="other" id="genders">
+                  <input type="radio" name="gender" value="other" id="other" />
+                  <span>Other</span>
+                </label>
+              </fieldset>
+            </div>
+            <div class="col offset-s7"></div>
+            <!--Email input-->
+            <div class="input-field col s6">
+              <label for="email">Email</label>
+              <input name="email" id="email" type="email" class="validate" required aria-required="true">
+            </div>
+            <div class="col offset-s6"></div>
+          </div>
+          <!--Countries select-->
+          <div class="row">
+            <div class="input-field col s4">
+              <?php include 'countries.php';?>
+              <div class="row">
+                <label for="countries">Country</label>
+              </div>
+              <div class="row">
+                <select name="countries" class="browser-default" required aria-required="true">
+                  <option name="select" value="" disabled selected>Please select your country</option>
+                  <?php 
+              foreach ($countries as $key => $country){
+              echo '<option name="'.$country.'"value="'.$key.'">'.$country.'</option>';
+              }
+            ?>
+                </select>
+              </div>
+            </div>
+            <div class="col offset-s8"></div>
+          </div>
+          <!--Subject select-->
+          <div class="row">
+            <div class="input-field col s3">
+              <div class="row">
+                <label for="subject">Subject</label>
+              </div>
+              <div class="row">
+                <select name="subject" class="browser-default">
+                  <option name="others" value="1" selected>Others</option>
+                  <option name="order" value="2">Order</option>
+                  <option name="technical_support" value="3">Technical support</option>
+                </select>
+              </div>
+            </div>
+            <div class="col offset-s9"></div>
+          </div>
+          <!--Message input-->
+          <div class="row">
+            <div class="input-field col s12">
+              <label for="message">Message</label>
+              <textarea name="message" id="message" class="materialize-textarea" required
+                aria-required="true"></textarea>
+            </div>
+          </div>
+          <!--Send button-->
+          <button class="btn waves-effect waves-light right" type="submit" name="action">Send message
+            <i class="material-icons right">send</i>
+          </button>
+        </form>
+      </div>
+    </div>
+  </main>
+  <!--Footer-->
+  <footer role=”contentinfo” class="page-footer">
+    <div class="container">
+      <div class="row">
+        <div class="col l6 s12">
+          <h5 class="white-text">Mailing list</h5>
+          <p class="grey-text text-lighten-4">We send out regular ipdates about what we're up to. If you'd like to
+            receive these straight to your mailbox, please enter your email address below.</p>
+          <div class="input-field col s9">
+            <label for="email_news" id="label_news">Email</label>
+            <input name="email_news" id="email_news" type="email" class="validate">
+          </div>
+          <button class="btn waves-effect waves-light right" id="button_news" type="submit" name="submit">
+            Submit
+          </button>
+        </div>
+        <div class="col l4 offset-l2 s12">
+          <h5 class="white-text">Links</h5>
+          <ul>
+            <li><a class="grey-text text-lighten-3" href="#!">
+                <div class="icon">
+                  <i class="fa fa-twitter circle blue"></i>
+                </div>
+              </a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">
+                <div class="icon">
+                  <i class="fa fa-facebook circle blue darken-4"></i>
+                </div>
+              </a></li>
+            <li><a class="grey-text text-lighten-3" href="#!">
+                <div class="icon">
+                  <i class="fa fa-youtube-play circle red accent-4"></i>
+                </div>
+              </a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-copyright">
+      <div class="container">
+        © 2014 Copyright Text
+        <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
+      </div>
+    </div>
+  </footer>
+
+  <!--JavaScript at end of body for optimized loading-->
+  <script type="text/javascript" src="js/bin/materialize.js"></script>
+</body>
+
+</html>
